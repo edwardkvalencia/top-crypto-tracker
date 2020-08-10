1b27ae316255374dee301b5a17a8eb8989bcbbfa
edwardkvalencia <edwardvalencia16@gmail.com>
2020-08-10 13:11:03 -0400

    componentization and prop drilling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,58 @@
 import React, { Component } from 'react'
-import logo from './logo.svg';
-import './App.css';
-import Coin from './components/Coin/Coin.jsx'
+import CoinList from './components/CoinList/CoinList.jsx'
 import AccountBalance from './components/AccountBalance/AccountBalance'
+import ExchangeHeader from './components/Header/Header'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="React Logo" className="App-logo"/>
-        <h1 className="App-title">
-          Coin Exchange
-        </h1>
-      </header>
+const Div = styled.div`
+  text-align: center;
+  background-color: yellow;
+  color: black;
 
-      <AccountBalance amount={10000} />
+`;
 
-      <table className="Coin-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Ticker</th>
-            <th>Price</th>
-          </tr>
-        </thead>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <tbody>
-          <Coin name="Bitcoin" ticker="BTC" price={9999.99} />
-          <Coin name="Ethereum" ticker="ETH" price= {299} />
-          <Coin name="Tether" ticker="USDT" price={1.00}/>
-          <Coin name="Ripple" ticker="XRP" price={.30} />
-        </tbody>
-      </table>
-      
-    </div>
-  );
+    this.state = {
+      balance: 10000,
+      coinData: [
+        { 
+          name: 'Bitcoin',
+          ticker:'BTC',
+          price: 9999.99
+        }, {
+          name: 'Ethereum',
+          ticker: 'ETH',
+          price: 299
+        }, {
+          name: 'Tether',
+          ticker: 'USDT',
+          price: 1
+        }, {
+          name: 'Ripple',
+          ticker: 'XRP',
+          price: 0.30
+        }, {
+          name: 'Bitcoin Cash',
+          ticker: 'BCH',
+          price: 299.99
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <Div className="App">
+        <ExchangeHeader />
+        <AccountBalance amount={this.state.balance} />
+        <CoinList coinData={this.state.coinData} />
+        
+      </Div>
+    );
+  }
 }
 
 export default App;
